d2268ccb48917f1e47110ef593d376f4a58dacac
Molkobain <lajarige.guillaume@free.fr>
2020-01-31 17:28:56 +0100

    Update module version to 1.1.0

--- module.itop-portal-url-brick.php
+++ module.itop-portal-url-brick.php
@@ -21,7 +21,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-portal-url-brick/1.1.0-dev',
+	'itop-portal-url-brick/1.1.0',
 	array(
 		// Identification
 		//
